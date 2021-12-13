91c0a8e4b20f8b9f471140674fdc95d2ffa0168e
theau <theau.trova@etu.univ-lyon1.fr>
2021-12-14 00:29:22 +0100

    load config

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 #!/bin/python3
-from billboard import BillBoard
+from src.billboard import BillBoard
+import argparse
 import yaml
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(prefix_chars='-')
+    parser.add_argument("-c", "--config",
+                            type=str,
+                            default="config.yaml",
+                            help="config file to load"
+                        )
+
+    args = parser.parse_args()
+    config_file = args.config
+
     billboard = BillBoard()
     
-    with open('config.yaml') as f:
+    with open(config_file) as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
+        for s in data["screens"]:
+            position = (s['position-on-wall']['x'], s['position-on-wall']['y'])
+            size = (s['size']['x'], s['size']['y']) if 'size' in s else None
+            billboard.register_screen( name=s['name'],
+                                       socket=s['socket'],
+                                       position=position,
+                                       orientation=s['orientation'] if 'orientation'in s else 0,
+                                       size = size if 'size' in s else None
+                                    )
         
         for z in data["zones"]:
-            print(z)
-            
-
-
-    billboard.add_carousel( folder="/home/theau/framebuffer-tests/1", screen=0, position=(  0,  0), size=(0.7,  1) )
-
-    billboard.run()
-    # billboard.update(20) # hold
+            size = (z['size']['x'], z['size']['y'])
+            position = (z['position']['x'], z['position']['y'])
+            billboard.register_zone( name=z['name'],
+                                       provider=z['provider'],
+                                       buffer=z['buffer'],
+                                       position=position,
+                                       size=size,
+                                       options=z['options'] if 'options'in z else {}
+                                    )
+        
+        billboard.run()
+        
+    
